14a7161c604ca0b9e9fa76e67da49339170bd7e1
ErjonStafa <erjonstafa647@gmail.com>
2022-02-22 10:24:46 +0100

    first commit

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,31 @@
+<div class="flex items-center text-base h-11 text-white bg-purple-800 font-sans">
+    <x-far-envelope class="w-4 h-4 ml-36 border-white bg-purple-800"/>
+    <div>
+        <a href="emailto:mhhasanul@gmail.com">mhhasanul@gmail.com</a>
+    </div>
+
+    <x-bx-phone-call class="w-4 h-4 border-white bg-purple-800"/>
+
+    <div>
+        <a href="tel:(12345)67890">(12345)67890</a>
+    </div>
+    <div>
+        English
+    </div>
+    <x-fas-angle-down class="w-4 h-4 border-white bg-purple-800" />
+    <div>
+        USD
+    </div>
+    <x-fas-angle-down class="w-4 h-4 border-white bg-purple-800" />
+    <div>
+        Login
+    </div>
+    <x-far-user class="w-4 h-4 border-white bg-purple-800" />
+    <div>
+        Wishlist
+    </div>
+    
+
+    <x-fas-cart-shopping class="w-5 h-5 border-purple-800 bg-purple-800" />
+</div>
+
